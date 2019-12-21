44c5ee3d9ce1f845eb35bb822b879dd7b3f4eca7
nsturgeon19 <nsturgeon19@dtechhs.org>
2019-12-21 13:13:13 -0700

    Created and printed random array

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.sql.Array;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        // The map is 10 x 10, with 2 spaces.
+        // First space = active units (soldiers)
+        // Second space = passive units (civilians/captives)
+        int[][][] mapNumbers = new int[10][10][2];
+
+        Random numGen = new Random();
+        for (int row = 0; row < mapNumbers.length; row++) {
+            for (int column = 0; column < mapNumbers[row].length; column++) {
+                for (int depth = 0; depth < mapNumbers[row][column].length; depth++) {
+                    mapNumbers[row][column][depth] = numGen.nextInt(100) + 1;
+                }
+            }
+        }
+
+        System.out.println(Arrays.deepToString(mapNumbers));
     }
 }
